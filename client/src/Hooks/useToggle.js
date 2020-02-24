ab5dae90f3aea2140b3e03325ba5941d9a7a31c8
Tyler Alsop <tylerjalsop@gmail.com>
2020-02-24 15:29:10 -0700

    Added a .light-mode class in App.css in case I decide to change my hook to a light-mode/dark-mode hook.

--- client/src/Hooks/useToggle.js
+++ client/src/Hooks/useToggle.js
@@ -1,16 +1,15 @@
 import React, {useState} from 'react';
 
-export const createList = (key, initialValues, cb) => {
+export const useToggle = (key, initialValues, cb) => {
   const [items, setItems] = useState([]);
-  
-  const newKeyValue = (object, newKey, newValue) => {
-    return object.newKey.newValue
-  };
 
+
+
+  
   const handleClick = e => {
     // console.log(e.target.value);
     setItems({
-      ...items, newKeyValue()
+      ...items
     });
     console.log(items);
   };
